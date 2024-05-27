e6e0f7529bb8ffbec0d5dd075844efb83f679a15
Jun Alcantara <junalcantara.dev@gmail.com>
2024-05-27 15:22:01 +0800

    Booking api

--- resources/views/site/shared-sections/booking-form.blade.php
+++ resources/views/site/shared-sections/booking-form.blade.php
@@ -1,87 +1,160 @@
 <section id="booking-form-section" class="bg-white">
   <div class="max-w-screen-xl mx-auto px-5 py-[50px]">
     <h3 class="text-4xl font-semibold text-[#083d67]">Book Now</h3>
-    <div class="mt-2 flex flex-col md:flex-row gap-[25px]">
-      <div class="md:basis-[50%]">
+    <div class="mt-2">
+      <div>
         <p class="mb-3">
           Experience Dental Care at its Finest! Visit and book at Dentmate Dental Clinic of your choice! 
         </p>
-        <div class="mb-3">
-          <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">First name:</label>
-          <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+        <div class="flex flex-col md:flex-row md:gap-[25px]">
+          <div class="basis-[50%]">
+            <div class="mb-3">
+              <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">First Name:</label>
+              <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+            </div>
+            <div class="mb-3">
+              <label for="middle_name" class="block mb-2 text-sm font-medium text-gray-900">Middle Name:</label>
+              <input type="text" id="middle_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+            </div>
+            <div class="mb-3">
+              <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900">Last Name:</label>
+              <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+            </div>
+            <div class="mb-3">
+              <label for="gender" class="block mb-2 text-sm font-medium text-gray-900">Gender:</label>
+              <select id="gender" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option selected>Select your gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="clinics" class="block mb-2 text-sm font-medium text-gray-900">Select Branch:</label>
+              <select id="clinics" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option selected>Choose Branch</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Note to Clinic:</label>
+              <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+            </div>
+          </div>
+          <div class="basis-[50%]">
+            <div class="mb-3">
+              <label for="mobile_number" class="block mb-2 text-sm font-medium text-gray-900">Mobile Number:</label>
+              <input type="text" id="mobile_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+            </div>
+            <div class="mb-3">
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email:</label>
+              <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
+            </div>
+            <div class="mb-3">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Select your date of visit:</label>
+              <input id="booking-date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Click here to select date">
+            </div>
+            <div class="mb-3">
+              <label for="slots" class="block mb-2 text-sm font-medium text-gray-900">Select your time slot:</label>
+              <select id="slots" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option selected>Select time slot</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div class="mb-3">
-          <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Last name:</label>
-          <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"required>
-        </div>
-        <div class="mb-3">
-          <label for="countries" class="block mb-2 text-sm font-medium text-gray-900">Select Branch:</label>
-          <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-            <option selected>Choose Branch</option>
-            <option value="fairview">Fairview</option>
-            <option value="pasig">Pasig</option>
-            <option value="san_juan">San Juan</option>
-            <option value="tandan_sora">Tandang Sora</option>
-          </select>
-        </div>
-        <div class="mb-3">
-        <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Note to Clinic:</label>
-        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
-        </div>
-        <div class="mb-3 flex justify-end">
-          <button class="bg-[#083d67] !px-[20px] !py-[10px] !text-[white] rounded-lg">
-            Book Now
-          </button>
-        </div>
-      </div>
-      <div class="basis-[50%]">
-        <h3 class="text-2xl text-[#083d67] mb-5 text-center font-semibold">When and what time?</h3>
-        <h5 class="text-center text-xl mb-2 font-semibold">January 2024</h5>
-        <div class="flex gap-[15px] justify-center mb-5">
-          <div class="!flex flex-col border-2 border-[#083d67] px-3 py-4 rounded-md cursor-pointer group hover:bg-[#083d67]">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">Wed</span>
-            <span class="text-center text-[#083d67] font-semibold text-[20px] group-hover:text-white">24</span>
-          </div>
-          <div class="!flex flex-col border-2 border-[#083d67] px-3 py-4 rounded-md cursor-pointer group hover:bg-[#083d67]">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">Thu</span>
-            <span class="text-center text-[#083d67] font-semibold text-[20px] group-hover:text-white">25</span>
-          </div>
-          <div class="!flex flex-col border-2 border-[#083d67] px-3 py-4 rounded-md cursor-pointer group hover:bg-[#083d67]">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">Fri</span>
-            <span class="text-center text-[#083d67] font-semibold text-[20px] group-hover:text-white">26</span>
-          </div>
-          <div class="!flex flex-col border-2 border-[#083d67] px-3 py-4 rounded-md cursor-pointer group hover:bg-[#083d67]">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">Sat</span>
-            <span class="text-center text-[#083d67] font-semibold text-[20px] group-hover:text-white">27</span>
-          </div>
-          <div class="!flex flex-col border-2 border-[#083d67] px-3 py-4 rounded-md cursor-pointer group hover:bg-[#083d67]">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">Sun</span>
-            <span class="text-center text-[#083d67] font-semibold text-[20px] group-hover:text-white">28</span>
-          </div>
-        </div>
-
-        <h5 class="text-center text-xl mb-2 font-semibold">Pick a Slot</h5>
-        <div class="flex gap-[15px] justify-center mb-5 flex-wrap">
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">10:00 AM</span>
-          </div>
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">11:00 AM</span>
-          </div>
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">12:00 NN</span>
-          </div>
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">01:00 PM</span>
-          </div>
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">02:00 PM</span>
-          </div>
-          <div class="basis-[25%] flex justify-center border-2 border-[#083d67] px-4 py-3 rounded-md group hover:bg-[#083d67] cursor-pointer">
-            <span class="text-center text-[#083d67] font-semibold group-hover:text-white">03:00 PM</span>
+        <div class="flex flex-col md:flex-row md:gap-[25px]">
+          <div class="mb-3 flex justify-end md:basis-[50%] pr-[10px]">
+            <button id="book-now-button" class="bg-[#083d67] !px-[20px] !py-[10px] !text-[white] rounded-lg">
+              Book Now
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="https://npmcdn.com/flatpickr@4.6.13/dist/themes/material_blue.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script>
+  const baseUrl = `http://3.141.43.249/api`
+  const clinicDropdown = $('#clinics')
+  const slotsDropdown = $('#slots')
+  const currentDate = new Date().toISOString().split('T')[0];
+  const scheduleMap = []
+  let availableDates = {}
+
+  $(document).ready(() => {
+    fetch(`${baseUrl}/available-clinics`)
+      .then(response => response.json())
+      .then((response) => {
+        let clinics = response.data.clinics
+
+        clinics.forEach(clinic => {
+          clinicDropdown.append(
+            `<option value="${clinic.id}">${clinic.name}</option>`
+          )
+        })
+      })
+  })
+
+  $('#clinics').change((e) => {
+    let selectedBranch = $(e.target).val()
+
+    fetch(`${baseUrl}/available-slots?clinic_id=${selectedBranch}`)
+      .then(response => response.json())
+      .then(response => {
+        let schedules = response.data.timeslots
+
+        schedules.forEach((date) => {
+          let scheduleDate = Object.keys(date)[0]
+          availableDates[scheduleDate] = Object.values(date)[0]
+        })
+
+        $("#booking-date").flatpickr({
+          enable: Object.keys(availableDates)
+        });
+      })
+  })
+
+  $('#booking-date').change((e) => {
+    let selectedDate = $(e.target).val()
+    let slots = availableDates[selectedDate]
+
+    slotsDropdown.html('')
+    slotsDropdown.append(`<option>Select time slot</option>`)
+
+    slots.forEach((slot) => {
+      slotsDropdown.append(
+        `<option value="${slot.timeslot_id}">${slot.start_time} to ${slot.end_time}</option>`
+      )
+    })
+  })
+
+  $('#book-now-button').click((e) => {
+    payload = {
+      firstname: $('#first_name').val(),
+      middlename: $('#middle_name').val(),
+      lastname: $('#last_name').val(),
+      gender: $('#gender').val(),
+      clinic_id: $('#clinics').val(),
+      remakrs: $('#message').val(),
+      mobile_no: $('#mobile_number').val(),
+      email: $('#email').val(),
+      timeslot_id: $('#slots').val()
+    }
+
+    fetch(`${baseUrl}/book-appointment`, {
+      method: 'POST',
+      data: payload
+    }).then(response => response.json())
+    .then((response) => {
+      console.log(response)
+    })
+  })
+
+  $("#booking-date").flatpickr({
+    dateFormat: "F j, Y",
+    enable: []
+  });
+</script>
