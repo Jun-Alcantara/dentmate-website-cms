6338d54bd685d3063215e13f4d0e560493dc6dd7
Jun Alcantara <junalcantara.dev@gmail.com>
2024-06-13 11:41:37 +0800

    Shorten the book now button

--- resources/views/layouts/site-layout.blade.php
+++ resources/views/layouts/site-layout.blade.php
@@ -29,7 +29,9 @@
           <span class="self-center text-2xl font-bold text-fontPrimary whitespace-nowrap"></span>
         </a>
         <div class="flex flex-row gap-[10px]">
-          <a href="#booking-form-section" class="md:hidden navigation-item bg-fontSecondary !px-[20px] !py-[10px] !text-[black] rounded-lg" aria-current="page">Book an appointment</a>
+          <a href="#booking-form-section" class="md:hidden navigation-item bg-fontSecondary !px-[20px] !py-[10px] !text-[black] rounded-lg" aria-current="page">
+            Book Now
+          </a>
           <button id="navbar-toggle" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
