b569c432b39e4372bd8620591059aeb23b4cc1ef
Jun Alcantara <junalcantara.dev@gmail.com>
2024-06-18 15:11:35 +0800

    Service Inner: Increase thumbnail width

--- resources/views/site/service-inner.blade.php
+++ resources/views/site/service-inner.blade.php
@@ -14,7 +14,7 @@
   <section class="bg-gray-100">
     <div class="max-w-screen-xl mx-auto py-5 px-5">
       <div class="flex flex-col md:flex-row">
-        <div class="mb-2 md:mr-3 basis-3/12">
+        <div class="mb-2 md:mr-3 basis-4/12">
           <img src="/storage/{{ $service->photo_url }}" class="w-full object-cover h-auto">
         </div>
         <div>
