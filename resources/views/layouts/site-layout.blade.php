d1b7c30d3755f76878b8b40c56059f3dfeee95e7
jun <jun@concept-machine.net>
2024-07-29 17:45:39 +0800

    New footer layout

--- resources/views/layouts/site-layout.blade.php
+++ resources/views/layouts/site-layout.blade.php
@@ -75,8 +75,8 @@
     </div>
 
     <section class="bg-[#083d67]">
-      <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center"> 
-        <ul class="flex flex-col md:flex-row text-center gap-[25px] my-5 md:px-5 md:my-10">
+      <div class="max-w-screen-xl mx-auto flex flex-col md:flex-row justify-between items-center py-5"> 
+        <ul class="order-2 text-left">
           <li>
             <a href="/about-us" class="text-white">About Us</a>
           </li>
@@ -90,19 +90,31 @@
             <a href="/#booking-form-section" class="text-white">Book Now</a>
           </li>
         </ul>
-        <div class="border-b-[1px] border-white w-full md:hidden"></div>
-        <ul class="my-5 px-5">
-          @foreach($branches as $branch)
-            <li class="text-white">
-              <svg class="inline h-[20px] w-[20px] mb-[5px]" xmlns="http://www.w3.org/2000/svg" viewBox="0,0,255.99098,255.99098">
-                <g fill="#083d67" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,255.99098v-255.99098h255.99098v255.99098z" id="bgRectangle"></path></g><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.016 4.432,10.984 10.206,11.852v-8.672h-2.969v-3.154h2.969v-2.099c0,-3.475 1.693,-5 4.581,-5c1.383,0 2.115,0.103 2.461,0.149v2.753h-1.97c-1.226,0 -1.654,1.163 -1.654,2.473v1.724h3.593l-0.487,3.154h-3.106v8.697c5.857,-0.794 10.376,-5.802 10.376,-11.877c0,-6.627 -5.373,-12 -12,-12z"></path></g></g>
-              </svg>
-              <a href="{{ $branch->facebook }}" target="_blank">
-                {{ $branch->name }}
-              </a>
-            </li>
-          @endforeach
-        </ul>
+        <div class="order-1">
+          <h3 class="uppercase text-white text-2xl text-left px-3">Explore More Smiles Here</h3>
+          <div>
+            <ul class="my-3 px-3 text-left">
+              @foreach($branches as $branch)
+                <li class="text-white">
+                  <svg class="inline h-[20px] w-[20px] mb-[5px]" xmlns="http://www.w3.org/2000/svg" viewBox="0,0,255.99098,255.99098">
+                    <g fill="#083d67" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,255.99098v-255.99098h255.99098v255.99098z" id="bgRectangle"></path></g><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.016 4.432,10.984 10.206,11.852v-8.672h-2.969v-3.154h2.969v-2.099c0,-3.475 1.693,-5 4.581,-5c1.383,0 2.115,0.103 2.461,0.149v2.753h-1.97c-1.226,0 -1.654,1.163 -1.654,2.473v1.724h3.593l-0.487,3.154h-3.106v8.697c5.857,-0.794 10.376,-5.802 10.376,-11.877c0,-6.627 -5.373,-12 -12,-12z"></path></g></g>
+                  </svg>
+                  <a href="{{ $branch->facebook }}" target="_blank">
+                    {{ $branch->name }}
+                  </a>
+                </li>
+              @endforeach
+            </ul>
+            <ul class="text-white text-left px-3 my-3">
+              <li>For urgent concerns, please call:</li>
+              <li>09920446404</li>
+              <li>09920446406</li>
+              <li>09942046171</li>
+              <li>09920446405</li>
+              <li class="mt-3">Inquiries for calls are open from 8AM to 5PM only. Thank you!</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
 
